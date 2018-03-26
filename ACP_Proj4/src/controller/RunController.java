519f71abf09c5c7b42caa0b804e959a324777cf8
Keenal <kms118@students.uwf.edu>
2018-03-26 14:21:56 -0500

    basic func done for controllers work for the 3 buttons

---
+++ ACP_Proj4/src/controller/RunController.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class RunController {
+	
+	public void RunButtonClicked() {
+		System.out.println("testing race button");
+	}
+	
+	
+	public void ResetButtonClicked() {
+		System.out.println("testing reset button");
+		
+	}
+	
+	public void QuitButtonClicked() {
+		System.out.println("Quitting the program.");
+		System.exit(0);
+	}
+	
+	
+
+}
